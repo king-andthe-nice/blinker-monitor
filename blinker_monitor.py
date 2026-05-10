84ac3edfdb43c8ca5903b0db18a04fbb0b84048b
king-andthe-nice <3356369582@qq.com>
2026-05-10 13:38:45 +0800

    Update blinker_monitor.py

--- blinker_monitor.py
+++ blinker_monitor.py
@@ -59,14 +59,36 @@ def send_wechat(title, content):
         print("❌ 微信消息发送失败", e)
 
 # ------------------- 读取 / 保存 上一次状态 -------------------
-# 获取上一次状态（从 GitHub 变量）
+# 读取上一次状态（成功读取）
 def get_last_state():
-    return os.getenv("LAST_STATE", None)
+    try:
+        repo = os.environ["GITHUB_REPOSITORY"]
+        token = os.environ["GITHUB_TOKEN"]
+        url = f"https://api.github.com/repos/{repo}/actions/variables/LAST_STATE"
+        headers = {
+            "Authorization": f"token {token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            return r.json()["value"]
+    except:
+        return None
 
+# 保存新状态（真正能修改成功）
 def save_state(state):
-    with open("GITHUB_ENV", "a") as f:
-        f.write(f"LAST_STATE={state}\n")
-
+    try:
+        repo = os.environ["GITHUB_REPOSITORY"]
+        token = os.environ["GITHUB_TOKEN"]
+        url = f"https://api.github.com/repos/{repo}/actions/variables/LAST_STATE"
+        headers = {
+            "Authorization": f"token {token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+        data = {"name": "LAST_STATE", "value": state}
+        requests.patch(url, json=data, headers=headers)
+    except:
+        pass
 # ------------------- 主逻辑 -------------------
 if __name__ == "__main__":
     print("="*50)
